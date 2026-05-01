167d603e38278bfd7729641ba4c10c7c19cc9402
demoarpile-cpu <demoarpile@gmail.com>
2026-05-01 16:00:48 +0530

    Update iclock.route.js

--- routes/iclock.route.js
+++ routes/iclock.route.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const iclockController = require("../controllers/iclock.controller");
-
+// 🟢 SIRF MACHINE ROUTES KE LIYE RAW TEXT ALLOW KARO
+router.use(express.text({ type: "/" })); 
 // ZKTeco ADMS (Push SDK) Endpoints
 router.all("/cdata", iclockController.handleCdata);
 router.get("/getrequest", iclockController.handleGetRequest);
 router.post("/devicecmd", iclockController.handleDeviceCmd);
-
-module.exports = router;
+module.exports = router;
