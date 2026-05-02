3e143d218e37602b9d26c4d2307e904b940c0979
demoarpile-cpu <demoarpile@gmail.com>
2026-05-02 15:09:48 +0530

    Update server.js
    sadas

--- server.js
+++ server.js
@@ -8,13 +8,29 @@ require('dotenv').config();
 const app = express();
 const server = http.createServer(app);
 
+
+// ================= CORS CONFIG =================
+const corsOptions = {
+    origin: [
+        "https://attendence.softwaredemolive.live"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+};
+
 // ================= SOCKET.IO =================
 const io = new Server(server, {
-    cors: { origin: "*" }
+    cors: {
+        origin: "https://attendence.softwaredemolive.live",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
 });
 
+
 // ================= MIDDLEWARE =================
-app.use(cors());
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 // Agar tum normal APIs me JSON bhi use karte ho
 app.use(express.json({ limit: '50mb' }));
@@ -25,6 +41,7 @@ const apiRoutes = require('./routes/api');
 
 // ⚠️ IMPORTANT: ZKTeco ke liye raw/text body chahiye, sirf /iclock routes par
 app.use('/iclock',
+    cors(), // 👈 ADD THIS (open for machine)
     express.text({
         type: ['text/plain', 'application/octet-stream'],
         limit: '50mb'
